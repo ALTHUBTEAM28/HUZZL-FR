cfcc7091eae2f0779eec2c7ddbf104de852b628e
Cello-Althub31 <celloalthubengineering@gmail.com>
2025-10-09 09:03:47 +0100

    navbar changes

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
+     
       <div className="navbar-logo">
         <Link to="/">Huzzl</Link>
       </div>
 
+     
       <ul className="navbar-links">
         <li>
           <Link to="/">Home</Link>
@@ -24,6 +32,7 @@ export default function Navbar() {
         </li>
       </ul>
 
+      
       <div className="navbar-buttons">
         <Link to="/login" className="btn login-btn">
           Log In
@@ -31,6 +40,39 @@ export default function Navbar() {
         <Link to="/CreateAccount" className="btn signup-btn">
           Sign Up
         </Link>
+      </div>
+
+      
+      <div className={`hamburger ${isOpen ? "open" : ""}`} onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+
+      
+      <div className={`mobile-menu ${isOpen ? "active" : ""}`}>
+        <ul>
+          <li>
+            <Link to="/" onClick={toggleMenu}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/services" onClick={toggleMenu}>
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={toggleMenu}>
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={toggleMenu}>
+              About us
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
